43d706e69d84c1b74c5aa9622a1a81432bbd29f4
tsubasa652 <tsuta906@gmail.com>
2022-06-03 19:15:37 +0900

    セッション管理など

--- src/jetOauth.js
+++ src/jetOauth.js
@@ -1,8 +1,15 @@
 const sessionManager = require("./utill/sessionManager")
+const cookie = require("cookie")
+const cookieSignature = require("cookie-signature")
 const fs = require("fs")
 const path = require("path")
 
 exports = module.exports = jetOauth
+
+function logout(res, session){
+    session.revoke()
+    res?.clearCookie("jetoauth")
+}
 
 function jetOauth(config = {}){
 
@@ -12,15 +19,15 @@ function jetOauth(config = {}){
         throw new Error("No providers found")
     }
 
+    const expire = config.expire || 7 * 24 * 60 * 60 * 1000
     const pluginsDir = config?.pluginsDir || path.join(__dirname, "plugins")
     const pluginFiles = fs.readdirSync(pluginsDir).filter(plugin => /\.js$/.test(plugin))
-
     const plugins = {}
 
     for(let pluginFile of pluginFiles){
         const plugin = require(path.join(pluginsDir, pluginFile))
         if(config.providers[plugin.provider]){
-            plugins[plugin.provider] = plugin(config.providers[plugin.provider])
+            plugins[plugin.provider] = plugin(config.providers[plugin.provider], cookie, expire)
         }
     }
 
@@ -28,6 +35,60 @@ function jetOauth(config = {}){
         if(req.methods == "OPTIONS"){
             return next()
         }
+
+        req.jetOauth = {
+            isLogin: false
+        }
+
+        if(req?.signedCookies?.jetoauth){
+            const session = new sessionManager(req.signedCookies.jetoauth)
+            if(session.get("expire") <= Date.now()){
+                req.jetOauth = {
+                    ...req.jetOauth,
+                    session,
+                    isLogin: true,
+                    logout: () => logout(res, session)
+                }
+            }
+        }
+
+        if(req.methods == "GET" && /^\/jetoauth\/(login|callback)\/(.+)$/.test(req.path)){
+            const provider = replace(req.path, /^\/jetoauth\/(login|callback)\//, "")
+            switch(true){
+                case /^\/jetoauth\/login\/(.+)$/.test(req.path):
+                    if(plugins[provider]){
+                        plugins[provider].login(req, res)
+                        return next(false)
+                    }
+                    break
+                case /^\/jetoauth\/callback\/(.+)$/.test(req.path):
+                    if(plugins[provider]){
+                        if(plugins[provider].callback(req, res)){
+                            res.redirect(config?.redirect?.success || "/jetOauth/success")
+                            return next(false)
+                        }else{
+                            res.redirect(config?.redirect?.fail || "/jetOauth/fail")
+                            return next(false)
+                        }
+                    }
+                    break
+            }
+        }
+        
+        if(req.methods == "GET" && /^\/jetoauth\/logout$/.test(req.path) && req.jetOauth.isLogin){
+            req.jetOauth.logout()
+            res.redirect(config?.redirect?.logout || "/")
+            return next(false)
+        }
+
+        if(!req.jetOauth.isLogin){
+            req.jetOauth = {
+                ...req.jetOauth,
+                login: (provider) => plugins[provider].login(req, res)
+            }
+        }
+
+        next()
     }
 }
 
